51ff760675198b9a17186630a9dfe8389abf8137
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-05 00:03:02 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,22 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+  it("filter by fav food", function(){
+		var listOfPeople = [
+			{
+				name: "Adam",
+				favoriteFood: "Grapes"
+			},
+			{
+				name: "Shauna",
+				favoriteFood: "Tacos"
+			}
+		];
+
+		var outcome = $filter("favoriteFood")(listOfPeople, "Tacos");
+
+		expect(outcome.length).toBe(1);
+		expect(outcome[0].name).toBe("Shauna");
+
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,9 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+it('should remove vowels', function(){
+	var outcome = $filter("removeAllVowels")("atom");
+	expect(outcome).toEqual("tm");
+});
+
 });
